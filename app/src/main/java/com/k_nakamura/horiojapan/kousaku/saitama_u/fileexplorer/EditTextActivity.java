61d165d985a163c9ba2bffdafd7f208ce2ff963c
KosukeNakamura <s15mh133@mail.saitama-u.ac.jp>
2015-12-08 21:12:47 +0900

    update
    text edit activity 追加(途中)

---
+++ app/src/main/java/com/k_nakamura/horiojapan/kousaku/saitama_u/fileexplorer/EditTextActivity.java
@@ -0,0 +1,70 @@
+package com.k_nakamura.horiojapan.kousaku.saitama_u.fileexplorer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class EditTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_text);
+
+        InputStream is = null;
+        BufferedReader br = null;
+        String text = "";
+
+        try {
+            try {
+                // assetsフォルダ内の sample.txt をオープンする
+                is = this.getAssets().open("sample.txt");
+                br = new BufferedReader(new InputStreamReader(is));
+
+                // １行ずつ読み込み、改行を付加する
+                String str;
+                while ((str = br.readLine()) != null) {
+                    text += str + "\n";
+                }
+            } finally {
+                if (is != null) is.close();
+                if (br != null) br.close();
+            }
+        } catch (Exception e){
+            // エラー発生時の処理
+        }
+
+// 読み込んだ文字列を EditText に設定し、画面に表示する
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.setText(text);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_edit_text, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
